5eec755c8e48c461e47bdac047603e2535eee58a
AkihisaForGit <oki.kaken.for.study@gmail.com>
2021-04-26 03:34:53 +0900

    20210426

--- import_psi4.py
+++ import_psi4.py
@@ -1,27 +1,68 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem.rdDistGeom import ETKDGv3, EmbedMolecule
+from rdkit.Chem.rdForceFieldHelpers import MMFFHasAllMoleculeParams, MMFFOptimizeMolecule
+from rdkit.Chem.Draw import IPythonConsole
 import psi4
+
 import datetime
 import time
 
-t = datetime.datetime.fromtimestamp(time.time())
+time
+#計算時間を見てみる
 
+# ハードウェア側の設定（計算に用いるCPUのスレッド数とメモリ設定）
 psi4.set_num_threads(nthread=2)
-psi4.set_memory('4GB')
-psi4.set_output_file('{}{}{}_{}{}.log'.format(t.year,
+psi4.set_memory("2GB")
+
+# 入力する分子（アセチルサリチル酸）
+smiles = 'CC(=O)Oc1ccccc1C(=O)O'
+
+# ファイル名を決める
+t = datetime.datetime.fromtimestamp(time.time())
+psi4.set_output_file("{}{}{}_{}{}_{}.log".format(t.year,
                                               t.month,
                                               t.day,
                                               t.hour,
-                                              t.minute)
+                                              t.minute,
+                                                smiles))
 
-### XYZ
-h2O_xyz = psi4.geometry('''
-0 1
-O 0 0　-0.11
-H 0 -1.4　1.2
-H 0 1.2 1.2
-''')
-### Z-matrix
-h2O_zmat = psi4.geometry('''
-0 1
-O
-H 1 0.96
-H 1 0.96 2 104.5''')
+
+# SMILES から三次元構造を発生させて、粗3D構造最適化
+mol = Chem.MolFromSmiles(smiles)
+mol = Chem.AddHs(mol)
+params = ETKDGv3()
+params.randomSeed = 1
+EmbedMolecule(mol, params)
+
+# MMFF（Merck Molecular Force Field） で構造最適化する
+MMFFOptimizeMolecule(mol)
+#UFF（Universal Force Field）普遍力場で構造最適化したい場合は
+#UFFOptimizeMolecule(mol)
+
+conf = mol.GetConformer()
+
+
+# Psi4 に入力可能な形式に変換する。
+# 電荷とスピン多重度を設定（下は、電荷０、スピン多重度1)
+mol_input = "0 1"
+
+#各々の原子の座標をXYZフォーマットで記述
+for atom in mol.GetAtoms():
+    mol_input += "\n " + atom.GetSymbol() + " " + str(conf.GetAtomPosition(atom.GetIdx()).x)\
+    + " " + str(conf.GetAtomPosition(atom.GetIdx()).y)\
+    + " " + str(conf.GetAtomPosition(atom.GetIdx()).z)
+
+molecule = psi4.geometry(mol_input)
+
+# 計算手法（汎関数）、基底関数を設定
+level = "b3lyp/6-31G*"
+
+# 計算手法（汎関数）、基底関数の例
+#theory = ['hf', 'b3lyp']
+#basis_set = ['sto-3g', '3-21G', '6-31G(d)', '6-31+G(d,p)', '6-311++G(2d,p)']
+
+# 構造最適化計算を実行
+energy, wave_function = psi4.optimize(level, molecule=molecule, return_wfn=True)
+
