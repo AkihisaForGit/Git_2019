10be77d345631b1a374cc0158bba794ebe694366
AkihisaForGit <oki.kaken.for.study@gmail.com>
2021-05-08 01:13:59 +0900

    20210508

--- chemoinfomatics/01_dinotefuran_TPSA.py
+++ chemoinfomatics/01_dinotefuran_TPSA.py
@@ -1,2 +1,16 @@
+from rdkit import Chem
 from rdkit.Chem import Descriptors
-Descriptors.TPSA(Chem.MolFromSmiles('CNC(=N[N+](=O)[O-])NCC1CCOC1')) #dinotefuran
+import psi4
+import datetime
+import time
+
+smiles = 'CNC(=N[N+](=O)[O-])NCC1CCOC1'
+Descriptors.TPSA(Chem.MolFromSmiles(smiles)) #dinotefuran
+
+t = datetime.datetime.fromtimestamp(time.time())
+psi4.set_output_file("{}_{}{}{}_{}{}.log".format(smiles,
+                                              t.year,
+                                              t.month,
+                                              t.day,
+                                              t.hour,
+                                              t.minute))
