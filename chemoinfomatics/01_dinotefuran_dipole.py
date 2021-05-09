793f43dff2fb239fb5e417e09fd9633279e2e73e
AkihisaForGit <oki.kaken.for.study@gmail.com>
2021-05-09 10:43:13 +0900

    20210509_2

--- chemoinfomatics/01_dinotefuran_dipole.py
+++ chemoinfomatics/01_dinotefuran_dipole.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem.rdDistGeom import ETKDGv3, EmbedMolecule
 from rdkit.Chem.rdForceFieldHelpers import MMFFHasAllMoleculeParams, MMFFOptimizeMolecule
 from rdkit.Chem.Draw import IPythonConsole
@@ -23,27 +24,27 @@ def smi2xyz(smiles):
     AllChem.EmbedMolecule(mol, AllChem.ETKDGv2())
     AllChem.UFFOptimizeMolecule(mol)
     conf = mol.GetConformer(-1)
-    
+
     xyz = '0 1'
     for atom, (x,y,z) in zip(mol.GetAtoms(), conf.GetPositions()):
         xyz += '\n'
         xyz += '{}\t{}\t{}\t{}'.format(atom.GetSymbol(), x, y, z)
-        
+
     return xyz
-    
+
 # 入力する分子（dinotefuran）
 
 smiles = 'CNC(=N[N+](=O)[O-])NCC1CCOC1'
 
 
-psi4.set_output_file('dinotefuran.txt')
+psi4.set_output_file('01_dinotefuran1.txt')
 dinotefuran = psi4.geometry(smi2xyz(smiles))
-_, wfn_dtf = psi4.optimize('hf/sto-3g', molecule=dinotefuran, return_wfn=True)
+_, wfn_dtf = psi4.optimize('B3LYP/6-31G*', molecule=dinotefuran, return_wfn=True)
 rdkit_dinotefuran = Chem.AddHs(Chem.MolFromSmiles(smiles))
 ## 双極子モーメントの計算
 
-psi4,oeprop(wfn_dtf, 'DIPOLE')
+psi4.oeprop(wfn_dtf, 'DIPOLE', titile='dipole')
 dipole_x, dipole_y, dipole_z = psi4.variable('SCF DIPOLE X'), psi4.variable('SCF DIPOLE Y'), psi4.variable('SCF DIPOLE Z') 
 dipole_moment = np.sqrt(dipole_x ** 2 + dipole_y ** 2 + dipole_z ** 2)
 
-print(round(dipole_moment,3),'D')
+#print(round(dipole_moment,3),'D')
